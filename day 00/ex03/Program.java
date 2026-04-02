3b2bc8744ca78d077339cf7de9929a5799daa8ca
nastkrash <nastkrash@gmail.com>
2026-04-02 14:53:10 -0500

    Fixes in day00

--- day 00/ex03/Program.java
+++ day 00/ex03/Program.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Program{
-    public static void main(String[] args){
+public class Program {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long storage = 0;
         int weekNumber = 0;
 
-        while(weekNumber != 18){
+        while (weekNumber != 18) {
             String weekLabel = sc.next();
             if (weekLabel.equals("42"))
                 break;
@@ -22,23 +22,22 @@ public class Program{
 
             sc.nextLine();// \n
 
-            storage = storage * 10  + minGrade(sc);
+            storage = storage * 10 + minGrade(sc);
             sc.nextLine();
         }
         printGraph(reverseNum(storage), weekNumber);
     }
 
-    public static void illegalExit(){
+    public static void illegalExit() {
         System.err.println("IllegalArgument");
         System.exit(-1);
     }
 
-    public static int minGrade(Scanner sc){
+    public static int minGrade(Scanner sc) {
         int min = 9;
         int count = 0;
 
-        while (count < 5)
-        {
+        while (count < 5) {
             if (!sc.hasNextInt())
                 illegalExit();
             if (sc.hasNext("42") && count < 5)
@@ -47,7 +46,7 @@ public class Program{
             int grade = sc.nextInt();
             count++;
 
-            if (grade < 1 || grade >9)
+            if (grade < 1 || grade > 9)
                 illegalExit();
             if (grade < min)
                 min = grade;
@@ -57,22 +56,23 @@ public class Program{
 
         return min;
     }
-    public static long reverseNum(long num){
+
+    public static long reverseNum(long num) {
         long result = 0;
-        while (num != 0){
+        while (num != 0) {
             result = result * 10 + (num % 10);
             num /= 10;
         }
         return result;
     }
-    public static void printGraph(long storage, int weeks){
-        for (int i = 1; i <= weeks; i++)
-        {
+
+    public static void printGraph(long storage, int weeks) {
+        for (int i = 1; i <= weeks; i++) {
             long grade = storage % 10;
             storage /= 10;
 
-            System.out.print("Week "+ i + " ");
-            System.out.print("=".repeat((int)grade));
+            System.out.print("Week " + i + " ");
+            System.out.print("=".repeat((int) grade));
             System.out.println(">");
         }
     }
